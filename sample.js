16397970357bffb9e6f4829aa87cc3d5aa397e19
satoshi kawaura <kanimo3104@gmail.com>
2022-01-28 01:36:58 +0900

    sample

---
+++ sample.js
@@ -0,0 +1,35 @@
+setInterval(() => {
+
+    var fs = require('fs');
+    var min = 0 ;
+    var max = 9999999999999 ;
+    var a = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+    
+    function writeFile(path, data) {
+    fs.writeFile(path, data, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+    }
+    
+    writeFile(${a}.txt, "test OK!");
+    
+    const exec = require('child_process').exec;
+    exec('git add . -A', (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+    console.log(stdout);
+    });
+    
+    exec('git commit -m "commit"', (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+    console.log(stdout);
+    });
+    
+    exec('git push origin main', (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+    console.log(stdout);
+    });
+    
+    console.log('push success');
+    }, 100);
